5068babb583cead1e52b290e641c9320f51bb59c
vitovtova ann <vitovtova.anya@mail.ru>
2022-02-13 16:43:13 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 1000; //стоимость билета
+        int mils = 10; // стоимость бонусной мили
+
+        System.out.println(amount / mils);
+
+    }
+}
